398fc9aded0e8fe280e6121680d4263f3e61a39b
EC2 Default User <ec2-user@ip-172-31-19-108.ec2.internal>
2024-09-21 16:56:25 +0000

    Organize config_bd

---
+++ config_bd/db_connection.php
@@ -0,0 +1,17 @@
+<?php
+function getDBConnection() {
+    $host = 'inst-db-papeleria-cvf1.c16ku0042y73.us-east-1.rds.amazonaws.com';
+    $dbname = 'empresa_actividades';
+    $user = 'jhonfre';
+    $password = 'DB_jhonfre';
+
+    try {
+        $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+    } catch (PDOException $e) {
+        echo 'Error: ' . $e->getMessage();
+        die();
+    }
+}
+?>
